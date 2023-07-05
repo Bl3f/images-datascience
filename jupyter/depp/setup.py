e731a712a087726d98aa1c7f70381c66328b70a0
Christophe Blefari <christophe.blefari@gmail.com>
2023-07-05 09:09:06 +0200

    trying again package depp

--- jupyter/depp/setup.py
+++ jupyter/depp/setup.py
@@ -7,6 +7,7 @@ setup(
     author='Christophe Blefari',
     author_email='christophe.blefari@gmail.com',
     url='https://blef.fr',
+    packages=['depp'],
     install_requires=[
         'requests',
         'boto3',
