26e5c18902898dcbdff2371eda314bff954e8015
Christophe Blefari <christophe.blefari@gmail.com>
2024-05-28 12:52:44 +0200

    Add scope as param in rstudio

--- nginx-sso/run.sh
+++ nginx-sso/run.sh
@@ -2,5 +2,6 @@ sed -i s/__KEYCLOAK_SECRET__/${KEYCLOAK_SECRET}/g /etc/nginx/conf.d/default.conf
 sed -i s/__KEYCLOAK_CLIENT_ID__/${KEYCLOAK_CLIENT_ID}/g /etc/nginx/conf.d/default.conf
 sed -i s/__KEYCLOAK_URL__/${KEYCLOAK_URL}/g /etc/nginx/conf.d/default.conf
 sed -i s/__RSTUDIO_URL__/${RSTUDIO_URL}/g /etc/nginx/conf.d/default.conf
+sed -i s/__SCOPE__/${__SCOPE__}/g /etc/nginx/conf.d/default.conf
 
 /usr/local/openresty/nginx/sbin/nginx -g "daemon off;"
